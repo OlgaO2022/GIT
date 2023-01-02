755c974e3bb94e2def588110cf85aa45ea90e99d
Olga <Olga@WIN-4B0FNU5FFBE>
2023-01-03 01:21:56 +0500

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main() {
+
+
+	// Задание 3
+
+	std::ifstream file("Text.txt");
+	if (!file.is_open()) {
+		std::cout << "error: file is not open!\n";
+		return -1;
+	}
+
+	int counter{};
+	std::string word;
+	std::string currentWord;
+	std::cout << "Enter any word: \n";
+	std::cin >> word;
+	while (!file.eof()) {
+		file >> currentWord;
+		{
+			if (currentWord == word) {
+				counter++;
+			}
+		}
+	}
+	std::cout << "Counter = " << counter << '\n';
+	file.close();
+
+	return 0;
+}
